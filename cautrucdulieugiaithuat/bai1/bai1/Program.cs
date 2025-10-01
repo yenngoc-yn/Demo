aa7b7b49f3df44dafd348ed198e94dd00d67e8b6
yenngoc-yn <cariyenngoc2002@gmail.com>
2025-10-01 08:56:37 +0700

    first commit

---
+++ cautrucdulieugiaithuat/bai1/bai1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            Console.WriteLine("Các phần tử sau khi Push: " + string.Join(", ", stack));
+
+            int removed = stack.Pop();
+            Console.WriteLine($"Phần tử bị Pop: {removed}");
+            Console.WriteLine("Stack sau khi Pop: " + string.Join(", ", stack));
+
+            int top = stack.Peek();
+            Console.WriteLine($"Phần tử đầu (Peek): {top}");
+
+            if (stack.Count == 0)
+                Console.WriteLine("Stack đang rỗng.");
+            else
+                Console.WriteLine("Stack KHÔNG rỗng.");
+        }
+    }
+}
